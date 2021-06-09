f16461dc37df6a761c1cd692b7b146300cf2c04a
Dwight Doane <dwightdoane@me.com>
2021-06-09 17:50:20 -0400

    this is a test

--- src/Replicant.elm
+++ src/Replicant.elm
@@ -5,5 +5,5 @@ import Html.Attributes as Attributes
 
 main =
   Html.p [Attributes.class "font-mono"] [
-  Html.text "Replicants are here"
+  Html.text "Replicants are here. I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched c-beams glitter in the dark near the Tannhuser Gate. All those moments will be lost in time, like tears in rain. Time to die."
   ]
