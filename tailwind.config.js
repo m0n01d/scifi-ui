03c7dfb49add76f879b07439d91e659b3deaca39
Dwight Doane <dwightdoane@me.com>
2021-06-10 17:39:27 -0400

    fix the folder pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   purge: [
     "./pages/**/*.{js,ts,jsx,tsx,elm}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/*.elm",
   ],
   darkMode: "media", // or 'media' or 'class'
   theme: {
