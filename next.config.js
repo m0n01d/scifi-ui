23ea84f1b4720bec3214e52ac4fd6337320d9ad4
Dwight Doane <dwightdoane@me.com>
2021-06-10 17:29:17 -0400

    wip

--- next.config.js
+++ next.config.js
@@ -1,22 +1,26 @@
 module.exports = {
   webpack: (config, options) => {
     const isProd = process.env.NODE_ENV === "production";
+
+    const elmLoader = {
+      loader: "elm-webpack-loader",
+      options: Object.assign(
+        {},
+        { optimize: isProd },
+        !isProd
+          ? {
+              runtimeOptions: ["-A128M", "-H128M", "-n8m"],
+              debug: true,
+            }
+          : {}
+      ),
+    };
     config.module.rules.push({
       test: /\.elm$/,
       exclude: [/elm-stuff/, /node_modules/],
-      use: {
-        loader: "elm-webpack-loader",
-        options: Object.assign(
-          {},
-          { optimize: isProd },
-          !isProd
-            ? {
-                runtimeOptions: ["-A128M", "-H128M", "-n8m"],
-                debug: true,
-              }
-            : {}
-        ),
-      },
+      use: isProd
+        ? elmLoader
+        : [{ loader: "elm-hot-webpack-loader" }, elmLoader],
     });
     return config;
   },
--- src/Replicant.elm
+++ src/Replicant.elm
@@ -1,9 +1,15 @@
-module Replicant exposing(main)
+module Replicant exposing (main)
 
 import Html
 import Html.Attributes as Attributes
 
+
 main =
-  Html.p [Attributes.class "font-mono"] [
-  Html.text "Replicants are here. I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched c-beams glitter in the dark near the Tannhuser Gate. All those moments will be lost in time, like tears in rain. Time to die."
-  ]
+    Html.div
+        [ Attributes.style "width" "1024px"
+        , Attributes.class "mx-auto border"
+        ]
+        [ Html.p [ Attributes.class "font-mono" ]
+            [ Html.text "Replicants are here"
+            ]
+        ]
